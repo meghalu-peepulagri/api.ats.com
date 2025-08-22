ec4b95272122a5ac5d1e4e5dba857a2ecf28db34
SatheeshDev8 <satheesh.m@orotron.com>
2025-08-22 14:56:56 +0530

    SK -- Add position/role filters

--- src/helper/applicantHelper.ts
+++ src/helper/applicantHelper.ts
@@ -11,15 +11,18 @@ export class ApplicantHelper {
 
     const search_string = query?.search_string || null;
     const status = query?.status || null;
+    const role = query?.role || null;
     if (search_string?.trim()) {
       const searchTerm = `%${search_string.trim().replace(/\s+/g, "%")}%`;
       filters.push(
         sql`(${applicants.first_name} ILIKE ${searchTerm} OR ${applicants.email} ILIKE ${searchTerm} OR ${applicants.phone} ILIKE ${searchTerm})`,
       );
     }
-
     if (status?.trim()) {
       filters.push(sql`${applicants.status} ILIKE ${`%${status}%`}`);
+    }
+    if (role?.trim()) {
+      filters.push(sql`${applicants.role} ILIKE ${`%${role}%`}`);
     }
     return filters;
   };
@@ -48,7 +51,7 @@ export class ApplicantHelper {
   };
 
   listApplicants = (query) => {
-    const filters: SQL[ ] = [];
+    const filters: SQL[] = [];
     const search_string = query?.search_string || null;
     const status = query?.status || null;
     if (search_string?.trim()) {
